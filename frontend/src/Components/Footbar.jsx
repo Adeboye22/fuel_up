d3c685e8191af7c016ec35e188d31bb79b0c2e1b
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-04-30 03:24:02 +0100

    Improved NavBar: added new styling to the navbar.

--- frontend/src/Components/Footbar.jsx
+++ frontend/src/Components/Footbar.jsx
@@ -2,29 +2,30 @@ import React from 'react'
 
 const Footbar = () => {
   return (
-    <div className='sm:flex sm:flex-col sm:gap-4 sm:p-8 sm:text-base p-8 flex flex-row text-xl justify-between px-14  text-altBlack'>
+    <div className='sm:flex sm:flex-col sm:gap-4 sm:p-8 sm:text-lg p-8 flex flex-row text-xl justify-between px-14  text-altBlack'>
         <div>
             <h3 className='font-black'>About</h3>
             <ul>
-                <li>About Us</li>
-                <li>Technology</li>
+                <a href="#about"><li>About Us</li></a>
                 <li>Contact Us</li>
             </ul>
         </div>
         <div>
             <h3 className=' font-black mb-2'>Services</h3>
             <ul>
-                <li>Fuel Dispensing</li>
-                <li>Payment Services</li>
-                <li>Event Services</li>
-                <li>Emergency Fuel delivery</li>
+                <a href="#fuel_dispensing"><li>Fuel Dispensing</li></a>
+                <a href="#availability"><li>24/7 Availability</li></a>
+                <a href="#accessibility"><li>Convenience & Accessibility</li></a>
+                <a href="#bulk_delivery"><li>Bulk Fuel Delivery</li></a>
+                <a href="#quality_assurance"><li>Quality Assurance</li></a>
+                <a href="#customer_care"><li>Customer Care</li></a>
             </ul>
         </div>
         <div>
             <h3 className='font-black mb-2'>Locations</h3>
             <ul>
-                <li>Lekki Phase 1</li>
-                <li>Lekki Phase 2</li>
+                <a href="#locations"><li>Lekki Phase 1</li></a>
+                <a href="#locations"><li>Lekki Phase 2</li></a>
             </ul>
         </div>
         <div>
