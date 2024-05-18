be28d4b72519459b16eafb47ffc49b4fb8c35c45
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-05-18 20:21:33 +0100

    App modification: added some packages and also modified the hero section, login, signup, etc

--- frontend/src/Components/website/Hero.jsx
+++ frontend/src/Components/website/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
 
   return (
       <div className='sm:my-4 sm:h-full sm:px-8 justify-center w-full h-3/4 px-44 my-40 sm:background-altBlack'>
-        <h1 className='font-bold text-4xl text-center'>Efficient fuel dispatching made simple:</h1>
-        <p className='my-4 text-2xl text-center'>Streamlining your operations with ease.</p>
+        <h1 className='sm:text-2xl font-bold text-4xl text-center'>Efficient fuel dispatching made simple:</h1>
+        <p className='sm:text-lg my-4 text-2xl text-center'>Streamlining your operations with ease.</p>
         <img src={Img} alt=""className='sm:hidden h-32 w-32 my-24 mx-64' />
       </div>
   )
