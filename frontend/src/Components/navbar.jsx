d1554bf821b60294e5ea871a037007e52126c251
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-05-20 22:41:21 +0100

    fixed abutton to login button

--- frontend/src/Components/navbar.jsx
+++ frontend/src/Components/navbar.jsx
@@ -13,6 +13,11 @@ const NavBar = () => {
     )
   }
 
+  const homeBtn = () => {
+    navigate('/')
+    window.scrollTo(0, 0);
+  }
+  
   const closeNavBar = () => {
     setIsOpen(false)
   }
@@ -26,7 +31,7 @@ const NavBar = () => {
   return (
     <div className='flex flex-row justify-between bg-white mt-4'>
         <div>
-            <NavLink to="/"><img src={Logo} className='sm:hidden md:hidden h-12 w-16 -my-8' /></NavLink>
+            <img src={Logo} className='sm:hidden md:hidden h-12 w-16 -my-8' />
         </div>
         <div className={`sm:fixed sm:top-4 sm:left-0 sm:bg-white sm:h-full sm:w-3/4 sm:text-base  sm:text-center sm:p-16 text-lg -my-4`}>
             <ul className='sm:flex sm:flex-col sm:gap-4 sm:my-36 text-gray flex flex-row gap-8 font-medium'>
