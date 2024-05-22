a95b4df9e1f8782c9bd68a3db9d3a25d4aa8a6fc
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-05-22 23:25:32 +0100

    more interface modifications

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -74,7 +74,7 @@ const Signup = () => {
   // }
 
   return (
-    <div className='pt-8'>
+    <div>
       <main className='bg-black p-12 min-h-screen'>
         <div className='flex flex-row justify-center'>
           <img src={Image} alt="" className='sm:my-12 sm:h-16 sm:w-16 h-20 w-20 border rounded'/>
