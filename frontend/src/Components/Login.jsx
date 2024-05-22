3bba52cc6e3dd2e26c20334c304ac8d83cba268d
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-05-22 23:47:02 +0100

    more interface modifications

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Validate from './ValidateLogin';
 import Image from '../assets/EcoFuel.gif';
+import { FaEnvelope, FaKey } from 'react-icons/fa';
 
 const Login = () => {
   const [value, setValue] = useState({
@@ -35,7 +36,7 @@ const Login = () => {
   // }
 
   return (
-    <div className='pt-12 h-full'>
+    <div className='pt-8 h-full'>
       <main className='sm:h-full sm:w-full bg-black p-20 min-h-screen pb-40'>
         <div className='sm:mt-24 flex flex-row justify-center'>
           <img src={Image} alt="" className='sm:h-12 sm:w-12 h-20 w-20 border rounded'/>
@@ -43,11 +44,19 @@ const Login = () => {
 
         <form  className='sm:mt-8 flex flex-col gap-4 pt-8'>
           <label htmlFor="email" className='text-white text-xl'>user email: </label>
-          <input type="email" name='email' onChange={handleInput} placeholder="youremail@gmail.com" className='sm:p-2 border border-gray rounded p-2'/>
+          <div className='sm:p-2 bg-white border rounded flex place-items-center gap-2'>
+            <FaEnvelope className='text-lime'/>
+            <input type="email" name='email' onChange={handleInput} placeholder="youremail@gmail.com" className='outline-none'/>
+          </div>
           {error.email && <span className = 'text-lime'>{error.email}</span>}
+
           <label htmlFor="" className='text-white text-xl'>password: </label>
-          <input type="password" name="password" onChange={handleInput} placeholder="*********" className='sm:p-2 border border-gray-100 rounded p-2'/>
+          <div className='sm:p-2 bg-white border rounded flex place-items-center gap-2'>
+            <FaEnvelope className='text-lime'/>            
+            <input type="password" name="password" onChange={handleInput} placeholder="*********" className='outline-none'/>
+          </div>
           {error.password && <span className = 'text-red'>{error.password}</span>}
+
           <div className='grid'>
             <button className='sm:w-20 sm:text-base text-white bg-lime p-2 text-xl border-white border-2 w-32 place-self-center rounded mt-4 font-bold justify-center' type='submit'>Login </button>
           </div>
--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -86,29 +86,29 @@ const Signup = () => {
           <h3 className='text-white'>First Name:</h3>
 
           <div className='sm:p-2 bg-white border rounded flex place-items-center gap-2'>
-          <FaUser className='text-lime'/>
-          <input type="text" name='firstname' onChange={handleInput} placeholder='first name' className='w-80 outline-none p-2' />
+            <FaUser className='text-lime'/>
+            <input type="text" name='firstname' onChange={handleInput} placeholder='first name' className='w-80 outline-none p-2' />
           </div>
           {errors.firstname && <span className="text-red">{errors.firstname}</span>}
 
           <h3 className='text-white'>Last Name:</h3>
           <div className='sm:p-2 bg-white border rounded flex place-items-center gap-2'>
-          <FaUser className='text-lime'/>
-          <input type="text" name='lastname' onChange={handleInput} placeholder='last name' className='w-80 outline-none p-2'/>
+            <FaUser className='text-lime'/>
+            <input type="text" name='lastname' onChange={handleInput} placeholder='last name' className='w-80 outline-none p-2'/>
           </div>
           {errors.lastname && <span className="text-red">{errors.lastname}</span>}
 
           <h3 className='text-white'>Email:</h3>
           <div className='sm:p-2 bg-white border rounded flex place-items-center gap-2'>
-          <FaEnvelope className='text-lime'/>
-          <input type="email" name='email' onChange={handleInput} placeholder='email' className='w-80 outline-none p-2'/>
+            <FaEnvelope className='text-lime'/>
+            <input type="email" name='email' onChange={handleInput} placeholder='email' className='w-80 outline-none p-2'/>
           </div>
           {errors.email && <span className = 'text-red'>{errors.email}</span>}
 
           <h3 className='text-white'>Password:</h3>
           <div className='sm:p-2 bg-white border rounded flex place-items-center gap-2'>
             <FaKey className='text-lime'/>
-          <input type="password" name='password' onChange={handleInput} placeholder='**********' className='w-80 outline-none p-2'/>
+            <input type="password" name='password' onChange={handleInput} placeholder='**********' className='w-80 outline-none p-2'/>
           </div>
           {errors.password && <span className = 'text-red'>{errors.password}</span>}
 
