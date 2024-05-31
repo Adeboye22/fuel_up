c40f8800ce7f2c91100eac61f0e904357449bfa3
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-05-31 22:38:29 +0100

    fix

--- frontend/src/Components/website/Pricing.jsx
+++ frontend/src/Components/website/Pricing.jsx
@@ -1,10 +1,12 @@
 // This is the codebase for Contact Us
 import React from 'react'
+import Marquee from 'react-fast-marquee'
 
 const ContactUs = () => {
   return (
     <div className='sm:gap-8 text-white flex flex-col gap-24'>
       <h1 className='sm:text-xl text-3xl font-black'>Pricing</h1>
+      <Marquee>
       <div className='sm:gap-12 sm:flex-col flex flex-row gap-24 text-lime justify-center text-lg'>
         <div className='flex flex-col'>
           <p>Petrol (PMS)</p>
@@ -18,8 +20,8 @@ const ContactUs = () => {
           <p className='text-lime'>Kerosene (AGO)</p>
           <p>N1450.00</p>
         </div>
-
       </div>
+      </Marquee>
     </div>
   )
 }
