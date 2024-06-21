ccc17c4e26630f65e200f8d9379e434f168deb66
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-06-21 06:37:16 +0100

    fuel up

--- frontend/src/Components/website/Pricing.jsx
+++ frontend/src/Components/website/Pricing.jsx
@@ -1,14 +1,16 @@
 // This is the codebase for Contact Us
 import React from 'react'
 import Marquee from 'react-fast-marquee'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const ContactUs = () => {
   return (
-    <div className='sm:gap-8 text-white bg-black p-8 h-screen'>
-      <h1 className='sm:text-xl text-3xl font-black pb-4'>Pricing</h1>
-      <p className='pb-8'>Our prices are standard, but flexible to variations. The prices below do not include delivery fee and service charge.</p>
+    <div data-aos='fade-up' className='sm:gap-8 text-white bg-black p-8 h-screen'>
+      <h1 className='sm:text-xl text-2xl font-black pb-4'>Pricing</h1>
+      <p className='sm:text-base pb-8 text-lg'>Our prices are standard, but flexible to variations. The prices below do not include delivery fee and service charge.</p>
       <Marquee>
-      <div className='sm:gap-4 sm:flex-col flex flex-row gap-24 text-lime justify-center text-lg'>
+      <div className='sm:text-base sm:gap-4 sm:flex-col flex flex-row gap-24 text-lime justify-center text-lg'>
         <div className='flex flex-col'>
           <p>Petrol (PMS)</p>
           <p className='text-red'>N665.00</p>
