abb4027f1e4b6d3b2253ae9bffa6646270af0195
Oluwadamilola Adeboye Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-09-08 06:48:51 +0100

    admin dashboard creation

--- frontend/src/Components/app/admin/admin.jsx
+++ frontend/src/Components/app/admin/admin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -11,33 +11,58 @@ const Admin = () => {
   }, []);
 
   return (
-    <div ata-aos="fade-up" className='h-screen text-gray bg-my-image bg-cover px-4'>
-      <div>
-        <h1>Welcome Adaripon!</h1>
+    <div data-aos="fade-up" className='h-screen text-gray bg-my-image bg-cover flex flex-col  gap-8 px-12 py-2'>
+      <div >
+        <h1 className='text-2xl font-bold text-gray'>Welcome Adaripon!</h1>
       </div>
 
-      {/* Customer orders */}
-      <div>
-        <h1>User Orders</h1>
-        <div className='p-8 bg-lime'></div>
+      {/* Customers orders */}
+      <div className='text-gray'>
+        <h1 className='text-lg font-semibold'>Customers Orders</h1>
+        <div className='p-8 flex flex-col gap-12 place-items-left'>
+          <select name="" id="" className='w-1/2 border rounded border-lime outline-none'></select>
+          <div><span>No users transaction found</span></div>
+        </div>
       </div>
 
       {/* Customer disputes */}
-      <div>
-        <h1>Disputes</h1>
-        <div className='p-8 bg-lime'></div>
+      <div className='text-gray'>
+        <h1 className='text-lg font-semibold'>Disputes</h1>
+        <div className='p-8 flex flex-col gap-12 place-items-left'>
+          <select name="" id="" className='w-1/2 border rounded border-lime outline-none'></select>
+          <div><span>No users transaction found</span></div>
+        </div>
       </div>
       
       {/* This is where the prices of fuel is adjusted*/}
-      <div>
-        <h1>Price Control</h1>
-        <div className='p-8 bg-lime'></div>
+      <div className='text-gray'>
+        <h1 className='text-lg font-semibold'>Price Control</h1>
+        <div className='p-8 flex flex-col gap-8 place-items-left'>
+          <div className='flex flex-row gap-2'>
+            <span>Petrol:</span>
+            <p>₦870</p>
+            <button className='bg-lime text-white px-2 border rounded'>Change</button> 
+          </div>
+          <div className='flex flex-row gap-2'>
+            <span>Diesel:</span>
+            <p>₦1250</p>
+            <button className='bg-lime text-white px-2 border rounded'>Change</button> 
+          </div>
+          <div className='flex flex-row gap-2'>
+            <span>Kerosene:</span>
+            <p>₦1250</p>
+            <button className='bg-lime text-white px-2 border rounded'>Change</button>          
+          </div>
+        </div>
       </div>
 
       {/* Employee data */}
-      <div>
-        <h1>Employee</h1>
-        <div className='p-8 bg-lime'></div>
+      <div className='text-gray'>
+        <h1 className='text-gray text-lg font-semibold'>Employee</h1>
+        <div className='p-8 flex flex-col gap-4 place-items-left'>
+          <span>No registered Employee</span>
+          <button className='bg-lime w-56 p-2 border rounded text-white'>create employee account</button>
+        </div>
       </div>
     </div>
   )
