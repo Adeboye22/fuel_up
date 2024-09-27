62d8aaf66fbfd847f0c1639ec09cd4b7b3c06ab8
Oluwadamilola Adeboye Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-09-27 23:14:35 +0100

    website correction

--- frontend/src/Components/Footbar.jsx
+++ frontend/src/Components/Footbar.jsx
@@ -6,7 +6,7 @@ const Footbar = () => {
     useEffect(() => {
         AOS.init({
           duration: 1000, // Animation duration
-          once: true, // Whether animation should happen only once - while scrolling down
+          once: false, // Whether animation should happen only once - while scrolling down
         });
       }, []);
   return (
--- frontend/src/Components/Webpage.jsx
+++ frontend/src/Components/Webpage.jsx
@@ -14,7 +14,7 @@ const Webpage = () => {
   useEffect(() => {
     AOS.init({
       duration: 1000, // Animation duration
-      once: true, // Whether animation should happen only once - while scrolling down
+      once: false, // Whether animation should happen only once - while scrolling down
     });
   }, []);
 
