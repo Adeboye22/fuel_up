bcc62bc4fc3e94361730d01313219ce80ac22e60
Oluwadamilola Adeboye Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-09-27 23:04:39 +0100

    website correction

--- frontend/src/Components/Footbar.jsx
+++ frontend/src/Components/Footbar.jsx
@@ -10,7 +10,7 @@ const Footbar = () => {
         });
       }, []);
   return (
-    <div data-aos="fade-up" className='sm:flex sm:flex-col sm:gap-4 sm:p-8 sm:text-lg p-8 flex flex-row text-xl justify-between px-14  text-altBlack'>
+    <div data-aos="fade-up" className='sm:flex sm:flex-col sm:gap-4 sm:p-8 sm:text-base p-8 flex flex-row text-xl justify-between px-14  text-altBlack'>
         <div>
             <h3 className='font-black'>About</h3>
             <ul>
