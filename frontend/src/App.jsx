3f20f4c9e59c31009916f6ca27f9b4dda5c79974
Oluwadamilola Adeboye Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-10-25 16:36:01 +0100

    NavBar: change Navbar in user login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,48 @@
-
+// components
 import { Routes, Route } from "react-router-dom";
-import Webpage from "./Components/Webpage";
-import Login from './Components/Login';
-import Signup from './Components/Signup';
 import Header from './Components/header';
 import Footbar from './Components/Footbar';
+import MaybeNavBar from "./Components/maybeNavBar";
+
+// pages
+import Webpage from "./Components/Webpage";
 import Services from './Components/website/Services';
-// import MailVerification from "./Components/mailVerification";
-import Pricing from "./Components/website/Pricing";
-// import User from "./Components/app/user/user";
-// import Petrol from "./Components/app/user/Petrol"
-// import Kero from "./Components/app/user/Kero"
-// import Diesel from "./Components/app/user/Diesel"
-// import History from "./Components/app/user/History"
-// import Admin from "./Components/app/admin/admin"
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import MailVerification from "./Components/mailVerification";
+// import Pricing from "./Components/website/Pricing";
+import User from "./Components/app/user/user";
+import Petrol from "./Components/app/user/Petrol"
+import Kero from "./Components/app/user/Kero"
+import Diesel from "./Components/app/user/Diesel"
+import History from "./Components/app/user/History"
+import Admin from "./Components/app/admin/admin"
 
 function App() {
 
   return (
     <>
-    <Header/>
+    <MaybeNavBar>
+      <Header/>
+    </MaybeNavBar>
     <Routes>
       <Route path='/' element={<Webpage/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<Signup/>} />
-      <Route path='/pricing' element={<Pricing/>} />
+      {/* <Route path='/pricing' element={<Pricing/>} /> */}
       <Route path='/services' element={<Services/>} />
-      {/* <Route path='/redirect_mail' element={<MailVerification/>} />
+      <Route path='/redirect_mail' element={<MailVerification/>} />
       <Route path='/admin' element={<Admin/>} />
       <Route path='/user' element={<User/>}>
         <Route path="orderPetrol" element={<Petrol/>} />
         <Route path="orderDiesel" element={<Diesel/>} />
         <Route path="orderKero" element={<Kero/>} />
         <Route path="transactions" element={<History/>} />
-      </Route> */}
+      </Route> 
     </Routes>
-    <Footbar/>
+    <MaybeNavBar>
+      <Footbar/>
+    </MaybeNavBar>
     </>
   )
 }
