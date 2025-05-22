295ac2f778a148eb780c4595129508e9030b2d43
Oluwadamilola Adeboye Oyeniya <oyeniyaoluwadamilola@outlook.com>
2025-05-22 20:25:58 +0100

    corrected components in frontend directory

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/navbar';
 import LoadingSpinner from '@/components/LoadingSpinner';
 
 const MainLayout = () => {
